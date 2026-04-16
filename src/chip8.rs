ff2c63635ce42b04202180529c5b412c75de7902
Pedro Aventuras <pedrocappelini@yahoo.com>
2026-04-16 16:45:57 -0300

    added display and enough instructions to run the IBM logo.ch8

--- src/chip8.rs
+++ src/chip8.rs
@@ -3,9 +3,9 @@ use std::io::Read;
 
 pub struct Chip8 {
     memory: [u8; 4096],
-    registers: [u8; 16],
+    register: [u8; 16],
     stack: [u16; 16],
-    display: [u8; 64 * 32],
+    pub display: [u8; 64 * 32],
     keypad: [u8; 16],
     index: u16,
     s_timer: u8,
@@ -43,7 +43,7 @@ impl Chip8 {
             .copy_from_slice(&FONTSET);
         Self {
             memory,
-            registers: [0; 16],
+            register: [0; 16],
             stack: [0; 16],
             display: [0; 64 * 32],
             keypad: [0; 16],
@@ -80,7 +80,79 @@ impl Chip8 {
         self.memory[START_ADRESS as usize..START_ADRESS as usize + buffer.len()]
             .copy_from_slice(&buffer);
 
-        dbg!(&self.memory[0x00..81]);
-        dbg!(&self.memory[0x200..0x20A]);
+        //dbg!(&self.memory[0x200..0x2F0]);
+    }
+
+    pub fn cycles(&mut self) {
+        //FETCH
+        let left_byte = self.memory[self.pc as usize];
+        let right_byte = self.memory[(self.pc + 1) as usize];
+        let opcode = u16::from_be_bytes([left_byte, right_byte]);
+        //println!("{:04X}", opcode);
+        self.pc += 2;
+
+        //DECODE
+        let n1 = (opcode & 0xF000) >> 12;
+        let n2 = (opcode & 0x0F00) >> 8;
+        let n3 = (opcode & 0x00F0) >> 4;
+        let n4 = opcode & 0x000F;
+
+        //EXECUTIONS
+        match (n1, n2, n3, n4) {
+            //0x00E0
+            (0x0, 0x0, 0xE, 0x0) => {
+                self.display.fill(0);
+            }
+            //0x6xnn
+            (0x6, _, _, _) => {
+                self.register[n2 as usize] = (n3 << 4) as u8 | n4 as u8;
+            }
+            //0xAnnn
+            (0xA, _, _, _) => {
+                self.index = (n2 << 8) | (n3 << 4) | n4;
+            }
+            //0x1nnn
+            (0x1, _, _, _) => {
+                self.pc = (n2 << 8) | (n3 << 4) | n4;
+            }
+            //0x7xnn
+            (0x7, _, _, _) => {
+                self.register[n2 as usize] =
+                    self.register[n2 as usize] + ((n3 << 4) as u8 | n4 as u8);
+            }
+            //0xDxyn
+            (0xD, vx, vy, n) => {
+                let x_pos = self.register[vx as usize] as usize % 64;
+                let y_pos = self.register[vy as usize] as usize % 32;
+                self.register[0xF] = 0; // Reset collision flag
+
+                for row in 0..n {
+                    let sprite_byte = self.memory[(self.index + row) as usize];
+                    if y_pos + row as usize >= 32 {
+                        break;
+                    }
+                    for col in 0..8 {
+                        if x_pos + col >= 64 {
+                            break;
+                        }
+                        let sprite_pixel = (sprite_byte >> (7 - col)) & 1;
+                        if sprite_pixel == 1 {
+                            let screen_idx = (x_pos + col) + ((y_pos + row as usize) * 64);
+                            if self.display[screen_idx] == 1 {
+                                self.register[0xF] = 1;
+                            }
+                            self.display[screen_idx] ^= 1;
+                        }
+                    }
+                }
+            }
+            //0xFFFF (doesn't exist on the chip8 architecture).
+            (0xF, 0xF, 0xF, 0xF) => {
+                self.display.fill(1);
+            }
+            (_, _, _, _) => {
+                print!(" . ");
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
 mod chip8;
 
 use chip8::Chip8;
+use macroquad::prelude::*;
 
-fn main() {
-    let mut chip_8 = Chip8::new();
+#[macroquad::main(window_config)]
+async fn main() {
+    let mut chip8 = Chip8::new();
 
-    chip_8.load_rom("roms/IBM Logo.ch8");
+    chip8.load_rom("roms/IBM Logo.ch8");
+    display_render(chip8).await;
+}
+
+async fn display_render(mut chip8: Chip8) {
+    loop {
+        chip8.cycles();
+
+        clear_background(BLACK);
+
+        dbg!(&chip8.display.len());
+
+        for y in 0..32 {
+            for x in 0..64 {
+                draw_rectangle(
+                    (x * 10) as f32,
+                    (y * 10) as f32,
+                    10.0,
+                    10.0,
+                    if chip8.display[y * 64 + x] == 1 {
+                        WHITE
+                    } else {
+                        BLACK
+                    },
+                );
+            }
+        }
+
+        next_frame().await
+    }
+}
+
+fn window_config() -> Conf {
+    Conf {
+        window_title: "Chip-8".to_string(),
+        window_width: 640,
+        window_height: 320,
+        ..Default::default()
+    }
 }
